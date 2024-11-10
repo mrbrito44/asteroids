6e26afa625c80f8e9bbd56aa57385ae7551e858d
mrbrito44 <joanbrito23@gmail.com>
2024-11-10 08:09:27 -0600

    setup screen game 2

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
-from module import function_hello, variable_player
+from constants import *
 
 def main():
-    print("Starting asteroids!")
-
+    while True:
+        pygame.init()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        color = pygame.Color(0, 0, 0,)
+        pygame.display.flip()
+        print("Starting asteroids!")
+        print(f"Screen width: {SCREEN_WIDTH} ")
+        print(f"Screen height: {SCREEN_HEIGHT} ")
 
 
 
